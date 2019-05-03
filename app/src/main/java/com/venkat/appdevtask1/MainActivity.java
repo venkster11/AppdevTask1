3f9ccc5efe4835c49a76835060cc6e3a9061d8d3
venkster11 <venkateshm11799@gmail.com>
2019-05-03 22:47:26 +0530

    Done t1

--- app/src/main/java/com/venkat/appdevtask1/MainActivity.java
+++ app/src/main/java/com/venkat/appdevtask1/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     String finalstring="";
-    int i=0;
+    int i=0,j=0;
+    int j1=0,j2=0,j3=0,j4=0,j5=0,j6=0,j7=0,j8=0,j9=0,j10=0,j11=0,j12=0,j13=0,j14=0,j15=0,j16=0;
     public Button btn1;
     public Button btn2;
     public Button btn3;
@@ -59,21 +60,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //v.setBackgroundColor(Color.GREEN);
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-           //     Toast.makeText(getBaseContext(), "This is my Toast message!",
-             //           Toast.LENGTH_LONG).show();
-                finalstring=finalstring+" Life ";
-                i=i+1;
-              //  Toast toast = Toast.makeText(getBaseContext(), finalstring , Toast.LENGTH_SHORT);
-               // toast.show();
+                j1=j1+1;
+                if(j1%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
+
             }
         });
+
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Politics ";
-                i=i+1;
+                j2=j2+1;
+                if(j2%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -81,8 +88,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Love ";
-                i=i+1;
+                j3=j3+1;
+                if(j3%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -90,8 +101,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Technology ";
-                i=i+1;
+                j4=j4+1;
+                if(j4%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -100,8 +115,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Money ";
-                i=i+1;
+                j5=j5+1;
+                if(j5%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -110,8 +129,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Career ";
-                i=i+1;
+                j6=j6+1;
+                if(j6%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -119,8 +142,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Sports ";
-                i=i+1;
+                j7=j7+1;
+                if(j7%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -128,8 +155,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Travel ";
-                i=i+1;
+                j8=j8+1;
+                if(j8%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -137,8 +168,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Health ";
-                i=i+1;
+                j9=j9+1;
+                if(j9%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -146,8 +181,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Adult ";
-                i=i+1;
+                j10=j10+1;
+                if(j10%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -155,8 +194,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Sports ";
-                i=i+1;
+                j11=j11+1;
+                if(j11%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+
+                }
             }
         });
 
@@ -164,8 +207,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Movies/TV ";
-                i=i+1;
+                j12=j12+1;
+                if(j12%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+                }
             }
         });
 
@@ -173,8 +219,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Books ";
-                i=i+1;
+                j13=j13+1;
+                if(j13%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+                }
             }
         });
 
@@ -182,8 +231,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Shopping ";
-                i=i+1;
+                j14=j14+1;
+                if(j14%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+                }
             }
         });
 
@@ -191,8 +243,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Gadgets ";
-                i=i+1;
+                j15=j15+1;
+                if(j15%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+                }
             }
         });
 
@@ -200,15 +255,85 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setBackgroundDrawable(getResources().getDrawable(R.drawable.colorchange));
-                finalstring=finalstring+" Web series ";
-                i=i+1;
+                j16=j16+1;
+                if(j16%2==0)
+                {
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundcornerbtn));
+                }
             }
         });
 
+        i=0;
         btnnxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if(j1%2!=0)
+                {
+                    finalstring=finalstring+" Life ";i++;
+                }
+                if(j2%2!=0)
+                {
+                    finalstring=finalstring+" Politics ";i++;
+                }
+                if(j3%2!=0)
+                {
+                    finalstring=finalstring+" Love ";i++;
+                }
+                if(j4%2!=0)
+                {
+                    finalstring=finalstring+" Technology ";i++;
+                }
+                if(j5%2!=0)
+                {
+                    finalstring=finalstring+" Money ";i++;
+                }
+                if(j6%2!=0)
+                {
+                    finalstring=finalstring+" Career ";i++;
+                }
+                if(j7%2!=0)
+                {
+                    finalstring=finalstring+" Sports ";i++;
+                }
+                if(j8%2!=0)
+                {
+                    finalstring=finalstring+" Travel ";i++;
+                }
+                if(j9%2!=0)
+                {
+                    finalstring=finalstring+" Health ";i++;
+                }
+                if(j10%2!=0)
+                {
+                    finalstring=finalstring+" Adult ";i++;
+                }
+                if(j11%2!=0)
+                {
+                    finalstring=finalstring+" Sports1 ";i++;
+                }
+                if(j12%2!=0)
+                {
+                    finalstring=finalstring+" Movies/TV ";i++;
+                }
+                if(j13%2!=0)
+                {
+                    finalstring=finalstring+" Books ";i++;
+                }
+                if(j14%2!=0)
+                {
+                    finalstring=finalstring+" Shopping ";i++;
+                }
+                if(j15%2!=0)
+                {
+                    finalstring=finalstring+" Gadgets ";i++;
+                }
+                if(j16%2!=0)
+                {
+                    finalstring=finalstring+" Web Series ";i++;
+                }
+
+                int j1=0,j2=0,j3=0,j4=0,j5=0,j6=0,j7=0,j8=0,j9=0,j10=0,j11=0,j12=0,j13=0,j14=0,j15=0,j16=0;
                 String fstr;
                 fstr="You have selected - "+finalstring;
                 if(i>=4) {
@@ -220,6 +345,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(getBaseContext(), "Select atleast 4 items", Toast.LENGTH_SHORT);
                     toast.show();
                 }
+               /* Toast toast = Toast.makeText(getBaseContext(), finalstring, Toast.LENGTH_SHORT);
+                toast.show();*/
+
+
+                finalstring="";i=0;
+
 
             }
         });
